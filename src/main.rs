b4f0f4e8b64ee04b4d92399ca08504f5561aebd3
yvcheta <cheta@youverify.co>
2021-11-06 18:01:00 +0100

    feat: finish first iteration of calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Please enter your weight in kg: ");
+    let mut user_weight_on_earth = String::new();
+
+    io::stdin().read_line(&mut user_weight_on_earth).unwrap();
+
+    let user_weight_on_earth = user_weight_on_earth.trim().parse::<f32>().unwrap();
+
+    let user_weight_on_mars = calculate_weight_on_mars(user_weight_on_earth);
+
+    println!("Your weight on mars is {}kg", user_weight_on_mars);
+}
+
+fn calculate_weight_on_mars(weight_on_earth: f32) -> f32 {
+    let mars_weight: f32 = (weight_on_earth / 9.81) * 3.711;
+
+    mars_weight
 }
